aa343d1565c8cf7adaf6abcabc248f98c47e4126
schoettker <lennartschoettker@hotmail.com>
2016-11-27 12:09:29 +0100

    add search field and button; when the button is clicked the value in the searchfield gets passed into a XMLHttpRequest to the wikipedia api; the response then gets appended to a list in the dom including the resulting titles and snippets

---
+++ script.js
@@ -0,0 +1,35 @@
+document.getElementById('button').addEventListener('click', function(event) {
+  retrieveData();
+  event.preventDefault();
+});
+function retrieveData() {
+  var request = new XMLHttpRequest();
+  request.addEventListener('load', listener);
+  request.open('GET', getUrl('search'));
+  // request.setRequestHeader('Origin',''
+  request.send();
+}
+function listener() {
+  var response = JSON.parse(this.responseText).query.search;
+  // console.log(response);
+  createList(response);
+}
+function getUrl(id) {
+  var searchTerm = document.getElementById(id).value;
+  // var url = 'https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srprop=snippet&srsearch=' + searchTerm + '&origin=*';
+  var url = 'https://en.wikipedia.org/w/api.php?action=query&format=json&srlimit=15&list=search&origin=*&srsearch=' + searchTerm;
+  return url;
+}
+function createList(objArr) {
+  var list = document.getElementById('list');
+  list.innerHTML = null;
+  objArr.forEach(function(object) {
+    var listitem = document.createElement('li'), title = document.createElement('h5'), para = document.createElement('p');
+    title.appendChild(document.createTextNode(object['title']));
+    // para.appendChild(document.createTextNode(object.snippet));
+    para.innerHTML = object.snippet;
+    listitem.appendChild(title);
+    listitem.appendChild(para);
+    list.appendChild(listitem);
+  });
+}
